22d9a657ad0308da5401198678501dd7f4a5772f
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-09-14 12:28:04 +0200

    Created basic welcome view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [nameExists, setNameExist] = useState(false); //fixa så att den kollar localstorage om det finns ett namn angivet redan
+
+  /*  
+ 
+  kalendern består av en array med 24 objekt  (en för varje dag till den 24:e dec)
+  varje object är alltså en dag
+
+class day{
+  name: number elr string (t. ex 1, 2, 3 osv)
+  hasBeenOpened: boolean (alla är false från början)
+  dateOfThisDay: date (borde vara date datatyp, vet dock inte för tillfället hur jag kan sätta ett specifikt datum för varje objekt)
+}  
+
+
+ */
+
   return (
     <>
-      <h1>Hello</h1>
+      {!nameExists && (
+        <article>
+          <h1>Välkommen till din meme adventskalender!</h1>
+          <h2>Fyll i ditt namn</h2>
+
+          <input type="text" placeholder="Jane Doe" />
+          <button>Bekräfta</button>
+        </article>
+      )}
+
+      {nameExists && (
+        <article>
+          <h1>"Ifyllt namns" adventskalender</h1>
+        </article>
+      )}
     </>
   );
 }
