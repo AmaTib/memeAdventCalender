c88905488109785b195bec19189871cf564e69e8
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-09-25 14:39:34 +0200

    Name set in localstorage from input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [nameExists, setNameExist] = useState(false); //fixa så att den kollar localstorage om det finns ett namn angivet redan
-
   /*  
- 
   kalendern består av en array med 24 objekt  (en för varje dag till den 24:e dec)
   varje object är alltså en dag
 
@@ -14,9 +11,9 @@ class day{
   hasBeenOpened: boolean (alla är false från början)
   dateOfThisDay: date (borde vara date datatyp, vet dock inte för tillfället hur jag kan sätta ett specifikt datum för varje objekt)
 }  
-
-
  */
+  const [nameExists, setNameExists] = useState(false); //fixa så att den kollar localstorage om det finns ett namn angivet redan
+  const [nameInput, setNameInput] = useState("");
 
   return (
     <>
@@ -25,14 +22,31 @@ class day{
           <h1>Välkommen till din meme adventskalender!</h1>
           <h2>Fyll i ditt namn</h2>
 
-          <input type="text" placeholder="Jane Doe" />
-          <button>Bekräfta</button>
+          <input
+            type="text"
+            placeholder="Jane Doe"
+            value={nameInput}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setNameInput(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              if (nameInput != "") {
+                localStorage.setItem("memeCalenderUserName", nameInput);
+                console.log(nameInput);
+                setNameExists(true);
+              }
+            }}
+          >
+            Bekräfta
+          </button>
         </article>
       )}
 
       {nameExists && (
         <article>
-          <h1>"Ifyllt namns" adventskalender</h1>
+          <h1>{nameInput + "s"} adventskalender</h1>
         </article>
       )}
     </>
