d8eba760c0a16cc34107730037ce58e7901a26f8
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-11-20 19:06:51 +0100

    bugfix: dont add an s if last letter is s

--- src/components/CalenderView.tsx
+++ src/components/CalenderView.tsx
@@ -7,12 +7,20 @@ interface ICalenderViewProps {
 }
 
 export const CalenderView = ({ nameExists, nameInput }: ICalenderViewProps) => {
+  const lastLetterinName = nameInput.charAt(nameInput.length - 1);
+
+  console.log(lastLetterinName);
+
   return (
     <>
       {nameExists && (
         <article>
           <h1>
-            <span className="redText">{nameInput + "s"}</span> adventskalender
+            <span className="redText">
+              {nameInput}
+              {lastLetterinName !== "s" ? "s " : " "}
+            </span>
+            adventskalender
           </h1>
 
           <AudioPLayer />
